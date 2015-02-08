711a9dff7ca1979b8e6abdaec64707e1de6bd8d1
Amitesh Singh <singh.amitesh@gmail.com>
2015-02-08 21:06:49 +0530

    deleted old RoR files.

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '811577fceb2ba96db1118da1d6ac5e917b7c2184a22af2e2d192c3f5b820b87f64878fef0494069780a0db6d788928df3961d36dd9ea79f5e98a328b9c291c66';
