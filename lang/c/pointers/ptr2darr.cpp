a658f2f06cdbafcdc64fff647390631d1654a8be
Amitesh Singh <singh.amitesh@gmail.com>
2015-09-01 23:00:55 +0530

    renamed

--- lang/c/pointers/ptr2darr.cpp
+++ lang/c/pointers/ptr2darr.cpp
@@ -19,6 +19,25 @@ int main()
    p++;
    printf("first element of 2nd row: %d\n", p[0][0]);
 
+
+   //This does not work well.. on delete, double free issue?
+   //How to dynamically allocate it?
+   /*
+   int (*dp)[3] = new int[1][3];
+   dp = a;
+   printf("first element: %d\n", dp[0][0]);
+
+   delete [] dp;
+   */
+
+   //one more way to allocate dynamically.
+   int (**dp2)[3] = new (int (*)[3]);
+   *dp2 = a;
+   printf("first element of dp2: %d\n", (*dp2)[0][0]);
+
+   delete dp2;
+   printf("\n");
+
    // pointer to an array of 2D integers 2*3
    int (*pp)[2][3] = &a; //It covers whole array
    printf("size of pp: %ld\n", sizeof pp);
