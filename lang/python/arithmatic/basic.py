ff164382f3b77edbd6706a0eabf0c55eaca90612
Amitesh Singh <singh.amitesh@gmail.com>
2016-03-06 13:03:14 +0530

    python: add basic example.

---
+++ lang/python/arithmatic/basic.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+a = 10
+b = 20
+
+c = a + b;
+
+print 'a+b = ', c
+
+if (c == (a + b)):
+   print 'c is equal to (a+b)'
+elif (c != 30):
+   print 'c is not equal to 30'
+
+count = 0
+while (count < 9):
+   print ' count is ', count
+   count = count + 1;
+
