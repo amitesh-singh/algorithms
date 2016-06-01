923fc625c0a1223c06f0eeb45d2f6c375f25c41d
Amitesh Singh <singh.amitesh@gmail.com>
2016-06-01 22:39:05 +0530

    Add template file

---
+++ algo/template.cpp
@@ -0,0 +1,21 @@
+//Use this file as template, just copy and paste
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+
+using namespace std;
+
+int T; //test cases
+int main()
+{
+  //write your test cases in sample.txt
+  freopen("sample.txt", "r", stdin);
+
+  cin >> T;
+  for (int t = 1; t <= T; ++t)
+  {
+    //Write your algorithm here
+  }
+
+  return 0;
+}
