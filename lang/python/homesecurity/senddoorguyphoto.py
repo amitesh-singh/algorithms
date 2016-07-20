4848a2a40e2035bc2696596da1931bef7acd0395
Amitesh Singh <singh.amitesh@gmail.com>
2016-07-20 19:26:08 +0530

    python: send door guy photo code

---
+++ lang/python/homesecurity/senddoorguyphoto.py
@@ -0,0 +1,48 @@
+""" Copyright (C) 2016 Amitesh Singh <singh.amitesh at gmail.com> """
+
+import urllib
+import time
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.MIMEBase import MIMEBase
+from email import encoders
+
+""" first dowload the photo"""
+urllib.urlretrieve ("http://192.168.0.105:8080/photo.jpg", "/tmp/photo.jpg")
+"""note: photoaf.jpg - take focused photo"""
+
+timeformat = "%H:%M:%S"
+dateformat = "%d/%m/%Y"
+
+timelog = time.strftime(timeformat) + " " + time.strftime(dateformat)
+
+fromaddr = "your email id"
+toaddr = "to email id"
+
+msg = MIMEMultipart()
+msg['From'] = fromaddr
+msg['To'] = toaddr
+msg['Subject'] = "Someone had visited to your door: " + timelog
+
+body = "Entry time & data:" + timelog
+
+msg.attach(MIMEText(body, 'plain'))
+
+filename = "photo.jpg"
+attachment = open("/tmp/photo.jpg", "rb")
+
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+msg.attach(part)
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(fromaddr, "your password")
+text = msg.as_string()
+server.sendmail(fromaddr, toaddr, text)
+server.quit()
