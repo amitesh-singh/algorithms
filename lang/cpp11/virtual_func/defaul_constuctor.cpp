491387f7065cfd002a52492bafdae627922fcebb
Amitesh Singh <singh.amitesh@gmail.com>
2017-08-31 20:19:29 +0900

    correct the documentation

--- lang/cpp11/virtual_func/defaul_constuctor.cpp
+++ lang/cpp11/virtual_func/defaul_constuctor.cpp
@@ -1,5 +1,8 @@
 /*
-   By default, C++ will provide a default constructor, copy constructor, copy assignment operator (operator=) and a destructor. If you provide alternate versions of any of these functions for your class, C++ will not provide a default version. However, in C++11, you can now specify that you would like the compiler to provide a default one anyway. This is done by prototyping the function and using the default specifier:
+   By default, C++ will provide a default constructor, copy constructor, copy assignment operator (operator=) and a destructor.
+    If you provide alternate versions of any of these functions for your class, C++ will not provide a default version.
+     However, in C++11, you can now specify that you would like the compiler to provide a default one anyway. 
+     This is done by prototyping the function and using the default specifier:
 
    class Foo
    {
