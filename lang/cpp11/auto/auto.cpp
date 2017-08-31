50ac5ae527c7cb42b735276b92e3a4599c8fcfa0
Amitesh Singh <singh.amitesh@gmail.com>
2017-08-31 21:19:19 +0900

    cpp11: update auto exaple

--- lang/cpp11/auto/auto.cpp
+++ lang/cpp11/auto/auto.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 using namespace std;
+template <typename T1, typename T2>
+auto compose(T1 t1, T2 t2) -> decltype(t1 + t2)
+{
+   return t1+t2;
+}
 
 // The auto keyword tells the compiler to infer the type of the variable from its initializer.
 int main()
@@ -10,7 +15,10 @@ int main()
    auto c = "hello"; // c - const char *
    auto a = x; // a - int
 
+   auto v = compose(2, 3.14); // v's type is double
+
    cout << c << endl;
+   cout << v << endl;
 
    return 0;
 }
