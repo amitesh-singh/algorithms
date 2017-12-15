28c0e14990514146aef5da1c615358942b490fc5
Amitesh Singh <singh.amitesh@gmail.com>
2017-12-15 15:38:05 +0900

    Add more examples

--- lang/lua/if.lua
+++ lang/lua/if.lua
@@ -1,5 +1,6 @@
 a = 10
 
+-- if example
 if (a < 20)
    then
       print("less than 20")
@@ -10,9 +11,22 @@ print("a:", a)
 a = 20
 print("a is changed to:", a)
 
+-- if ..else example
 if (a < 20)
    then
       print("less than 20")
    else
       print("greater than 20")
    end
+
+-- if .. elseif example
+if (a == 10)
+   then
+      print("a is 10")
+elseif (a == 20)
+   then
+      print("a is 20")
+else
+   print("its a:", a)
+end
+
