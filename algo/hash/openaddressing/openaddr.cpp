32771e8ce92803d736d7f3d686fd74f18a1bce84
Amitesh Singh <singh.amitesh@gmail.com>
2018-09-15 07:00:27 +0530

    hash: openaddressing - make it little better.

--- algo/hash/openaddressing/openaddr.cpp
+++ algo/hash/openaddressing/openaddr.cpp
@@ -7,179 +7,181 @@ using namespace std;
 
 enum
 {
-    ADD = 100,
-    MOD = 200,
-    DEL = 300,
-    GET_VAL = 400
+   ADD = 100,
+   MOD = 200,
+   DEL = 300,
+   GET_VAL = 400
 };
 
 #define HASHTABLESIZE 1001
 
 unsigned long hashfunc(char *str)
 {
-    unsigned long hashVal = 5381;
-    int c;
+   unsigned long hashVal = 5381;
+   int c;
 
-    while ((c = *str++))
-        hashVal = (hashVal << 5) + hashVal + c;
+   while ((c = *str++))
+     hashVal = (hashVal << 5) + hashVal + c;
 
-    return hashVal;
+   return hashVal;
 }
 
 int hashmod(unsigned long key)
 {
-    return key % HASHTABLESIZE;
+   return key % HASHTABLESIZE;
 }
 
-struct hashtable{
-char keyhash[1001*2][50] = {0};
-int valhash[1001*2] = {0};
-int statushash[1001*2] =  {0};
+struct hashtable
+{
+   char keyhash[50] = {0};
+   int valhash = {0};
+   int statushash =  {0};
 };
 
-hashtable hashTable;
+hashtable hashTable[HASHTABLESIZE*5];
 
 int getIndex(int initialIndex)
 {
-    int finalIndex = initialIndex;
+   int finalIndex = initialIndex;
 
-    while (hashTable.statushash[finalIndex] == 1)
-    {
+   while (hashTable[finalIndex].statushash == 1)
+     {
+        //linear probe
         finalIndex = (finalIndex + finalIndex) % HASHTABLESIZE;
         if (finalIndex == initialIndex)
-        {
-            cerr << "Table is full\n" << endl;
-        }
-    }
+          {
+             cerr << "Table is full\n" << endl;
+          }
+     }
 
-    return finalIndex;
+   return finalIndex;
 }
 
 void insert(char *key, int element)
 {
-    int index = hashmod(hashfunc(key));
+   int index = hashmod(hashfunc(key));
 
-    int finalIndex = getIndex(index);
+   int finalIndex = getIndex(index);
 
-    hashTable.statushash[finalIndex] = 1;
-    hashTable.valhash[finalIndex] = element;
-    strcpy(hashTable.keyhash[finalIndex], key);
+   hashTable[finalIndex].statushash = 1;
+   hashTable[finalIndex].valhash = element;
+   strcpy(hashTable[finalIndex].keyhash, key);
 }
 
 int search(char *key)
 {
-    int index = hashmod(hashfunc(key));
+   int index = hashmod(hashfunc(key));
 
-    int finalIndex = index;
+   int finalIndex = index;
 
-    while (hashTable.statushash[finalIndex] == 1)
-    {
-        if (!strcmp(hashTable.keyhash[finalIndex], key))
-        {
-            break;
-        }
-        
+   while (hashTable[finalIndex].statushash == 1)
+     {
+        if (!strcmp(hashTable[finalIndex].keyhash, key))
+          {
+             break;
+          }
+
         finalIndex = (finalIndex + finalIndex) % HASHTABLESIZE;
         if (finalIndex == index)
-        {
-            cerr << "Not found" << endl;
-            return -1;
-        }
-    }  
+          {
+             cerr << "Not found" << endl;
+             return -1;
+          }
+     }
 
-    return finalIndex;  
+   return finalIndex;
 }
 
 void mod(char *key, int n)
 {
-    int index = search(key);
+   int index = search(key);
 
-    if (index == -1)
-        return;
-    hashTable.valhash[index] = n;
-    strcpy(hashTable.keyhash[index], key);
+   if (index == -1)
+     return;
+   hashTable[index].valhash = n;
+   strcpy(hashTable[index].keyhash, key);
 }
 
 int get(char *key)
 {
-    int index = search(key);
+   int index = search(key);
 
-    if (index == -1)
-        return 0;
-    
-    return hashTable.valhash[index];
+   if (index == -1)
+     return 0;
+
+   return hashTable[index].valhash;
 }
 
 void del(char *key)
 {
-    int index = search(key);
+   int index = search(key);
 
-    if (index == -1)
-        return;
-    hashTable.statushash[index] = 0; //empty it
+   if (index == -1)
+     return;
+   hashTable[index].statushash = 0; //empty it
 }
 
 void dumpHash()
 {
-    for (int i = 0; i <= 1000; ++i)
-    {
-        if (hashTable.statushash[i])
-        {
-            cout << hashTable.keyhash[i] << "=" << hashTable.valhash[i] << endl;
-        }
-    }
+   for (int i = 0; i <= 1000; ++i)
+     {
+        if (hashTable[i].statushash)
+          {
+             cout << hashTable[i].keyhash << "=" << hashTable[i].valhash << endl;
+          }
+     }
 }
 
 int main()
 {
-    freopen("input.txt", "r", stdin);
-    int T;
-    cin >> T;
-    int N;
-    int n;
-    char key[13];
-    int command;
-    int val;
+   freopen("input.txt", "r", stdin);
+   int T;
+   cin >> T;
+   int N;
+   int n;
+   char key[13];
+   int command;
+   int val;
 
-    for (int t = 1; t <= T; ++t)
-    {
+   for (int t = 1; t <= T; ++t)
+     {
         cin >> N;
         for (int i = 0; i < N; ++i)
-        {
-            cin >> command;
-            
-            switch(command)
-            {
+          {
+             cin >> command;
+
+             switch(command)
+               {
                 case ADD:
-                cin >> key;
-                cin >> n;
-                insert(key, n);
-                break;
-                
+                   cin >> key;
+                   cin >> n;
+                   insert(key, n);
+                   break;
+
                 case MOD:
-                cin >> key;
-                cin >> n;
-                mod(key, n);
-                break;
-                
+                   cin >> key;
+                   cin >> n;
+                   mod(key, n);
+                   break;
+
                 case DEL:
-                cin >> key;
-                del(key);
-                break;
+                   cin >> key;
+                   del(key);
+                   break;
 
                 case GET_VAL:
-                cin >> key;
-                val = get(key);
-                cout << val << endl;
-                break;
+                   cin >> key;
+                   val = get(key);
+                   cout << val << endl;
+                   break;
 
                 default:
-                ;
-            }
-            
-        }
-        dumpHash();
-    }
+                   ;
+               }
 
-    return 0;
+          }
+        dumpHash();
+     }
+
+   return 0;
 }
