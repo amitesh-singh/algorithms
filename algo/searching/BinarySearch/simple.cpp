95eb3adf44a00607a202836b7cba678a3c2541bc
Amitesh Singh <singh.amitesh@gmail.com>
2018-10-13 09:37:46 +0530

    search: binary - Add simple example along with lower and upper bound.

---
+++ algo/searching/BinarySearch/simple.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+
+using namespace std;
+
+int binarySearch(int a[], int low, int high, int key)
+{
+    int mid;
+
+    while (low <= high)
+    {
+        mid = low + (high - low)/2;
+        if (key < a[mid])
+            high = mid - 1;
+        else if (key > a[mid])
+            low = mid + 1;
+        else
+            return mid;
+    }
+
+    return -1;
+}
+
+//used for finding range
+int lowerbound(int a[], int low, int high, int key)
+{
+    int mid;
+    while (low <= high)
+    {
+        mid = low + (high - low)/2;
+        if (key < a[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    return low;
+}
+
+int higherbound(int a[], int low, int high, int key)
+{
+    int mid;
+
+    while (low <= high)
+    {
+        mid = low + (high - low)/2;
+        if (key < a[mid])
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    return high;
+}
+
+void findRange(int a[], int low, int high, int leftVal, int rightVal)
+{
+    int left = lowerbound(a, low, high, leftVal);
+    int right = higherbound(a, left, high, rightVal);
+
+    cout << "(" << left << "," << right << ")" << endl;
+}
+
+int main()
+{
+    int a[] = {1, 3, 4, 5, 9, 10, 11, 43, 45, 56, 89, 90, 91, 97};
+    int n = sizeof(a)/sizeof(int);
+
+    cout << "find 11: " << binarySearch(a, 0, n - 1, 11) << endl;
+    cout << "find 97 :" << binarySearch(a, 0, n -1, 97) << endl;
+    
+    cout << "lowerbound(45): " << a[lowerbound(a, 0, n - 1, 45)] << endl;
+    cout << "higher bound(92) " << a[higherbound(a, 0, n - 1, 92)] << endl;
+
+    findRange(a, 0, n - 1, 45, 92);
+
+    return 0;   
+}
