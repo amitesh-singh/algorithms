4ec5375fa54d9173246db146d4356c2f94f315dc
Amitesh Singh <singh.amitesh@gmail.com>
2019-07-27 19:02:03 +0530

    c++11: thread: fix the example

--- lang/cpp11/thread/PassArgsToThread.cpp
+++ lang/cpp11/thread/PassArgsToThread.cpp
@@ -3,7 +3,7 @@
 //how to pass args to a thread
 using namespace std;
 
-void func(int a,string &s)
+void func(int a,string s)
 {
     cout << a << ":" << s <<endl;
 }
@@ -11,7 +11,9 @@ void func(int a,string &s)
 int main()
 {
     string google("ok");
-    thread t(func,10,google);
+
+    thread t(func, 10, google);
     t.join();
+
     return 0;
 }
