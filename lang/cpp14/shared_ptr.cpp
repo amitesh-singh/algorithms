9ddf491206c58538c5679a0caa6414965b4da5e8
Amitesh Singh <singh.amitesh@gmail.com>
2020-01-31 21:56:40 +0530

    Add shared ptr example

---
+++ lang/cpp14/shared_ptr.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory> //for shared_ptr
+
+using namespace std;
+
+int main()
+{
+   shared_ptr<int> sp1 (new int());
+   *sp1 = 100;
+
+   cout << sp1 << ": " << *sp1 << endl;
+
+   // Error-->shared_ptr<int> sp2 = new int(100);
+   // instead use make_shared<int>();
+   shared_ptr<int> sp2 = std::make_shared<int>();
+   *sp2 = -100;
+   cout << sp2 << ": " << *sp2 << endl;
+
+   //resetting
+   sp1.reset(); //deletes the ptr if ref count is zero
+
+   cout << "ref count = " << sp1.use_count() << endl;
+
+   //resetting previous value and adding new one
+   sp2.reset(new int(32));
+   cout << "ref count = " << sp2.use_count() << endl;
+
+   //resetting by nullptr.
+   //sp2 = nullptr
+   sp2 = nullptr;
+   cout << "ref count = " << sp2.use_count() << endl;
+
+   return 0;
+}
