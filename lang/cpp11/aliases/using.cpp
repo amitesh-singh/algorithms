9587bdde6f130b3fc3c979b9de60cb9717e05b9e
Amitesh Singh <singh.amitesh@gmail.com>
2020-02-01 16:09:34 +0530

    cpp11: using exampel

---
+++ lang/cpp11/aliases/using.cpp
@@ -0,0 +1,40 @@
+/*
+ *Semantically similar to using a typedef however, type aliases with using are easier to read and are compatible with templates.
+
+template <typename T>
+using Vec = std::vector<T>;
+Vec<int> v; // std::vector<int>
+
+using String = std::string;
+String s {"foo"};
+*/
+
+#include <iostream>
+
+using namespace std;
+
+using String = std::string;
+
+class BigNameClass
+{
+ public:
+    BigNameClass(String name)
+      {
+         cout << name << endl;
+      }
+};
+
+using BNC = BigNameClass;
+
+int main()
+{
+   String ami("Amitesh Singh");
+   cout << ami << "\n";
+
+   //can be used for class too.
+   //
+   BNC bnc(ami);
+
+   return 0;
+}
+
