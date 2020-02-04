32a22b590db705e9d8c69fc8956b6594fa77fe4b
Amitesh Singh <singh.amitesh@gmail.com>
2020-02-04 07:00:38 +0530

    make it better

--- lang/cpp11/nullptr/nullptr.cpp
+++ lang/cpp11/nullptr/nullptr.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <type_traits>
 
 using namespace std;
+
+#define F cout << __PRETTY_FUNCTION__ << endl
+
+void foo(int x)
+{
+   F;
+}
+
+void foo(char *x)
+{
+   F;
+}
 
 int main()
 {
@@ -9,6 +22,17 @@ int main()
    int *ptr = nullptr;
 
    cout << p << " nullptr: " << ptr << endl;
+   if (std::is_same<decltype(NULL), decltype(0)>::value)
+     cout << "NULL == 0" << endl;
+   if (std::is_same<decltype(NULL), decltype((void *)0)>::value)
+     cout << "NULL == (void *)0" << endl;
+   if (std::is_same<decltype(NULL), std::nullptr_t>::value)
+     cout << "NULL == nullptr_t" << endl;
+   if (std::is_same<decltype(nullptr), std::nullptr_t>::value)
+     cout << "nullptr is of nullptr_t type" << endl;
 
+  // foo(NULL); //compilation error, ambiguous call..
+   foo(0);
+   foo(nullptr);
    return 0;
 }
