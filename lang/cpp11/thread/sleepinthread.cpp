7af621e094ec8b4da629d5311300380145c71ca8
Amitesh Singh <singh.amitesh@gmail.com>
2020-02-23 19:30:59 +0530

    Add sleep example in thread

---
+++ lang/cpp11/thread/sleepinthread.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+void func()
+{
+    while (1)
+    {
+        //1s timeout
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        std::cout << "thread called.\n";
+    }
+
+}
+
+int main()
+{
+    std::thread t1(func);
+    t1.detach();
+    while (1)
+    {
+        //gonna run forever
+
+    }
+    return 0;
+}
