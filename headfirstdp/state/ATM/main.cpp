d10297697a7152029261f209a3da91ef77414b5b
Amitesh Singh <singh.amitesh@gmail.com>
2020-02-23 13:39:24 +0530

    add

--- headfirstdp/state/ATM/main.cpp
+++ headfirstdp/state/ATM/main.cpp
@@ -6,12 +6,23 @@ int main()
     atm.insertCard();
     atm.insertPinAndWithdrawCash(1234, 100);
 
-    cout << "ATM balance: " << atm.getBalance() << endl;
+    cout << "ATM balance: " << atm.getBalance() << endl << "\n";
 
     atm.insertCard();
     atm.ejectCard();
-
     atm.insertPinAndWithdrawCash(1234, 100);
+    
+    cout << "ATM balance: " << atm.getBalance() << endl<< "\n";
+
+    atm.insertCard();
+    atm.insertPinAndWithdrawCash(1245, 100);
+    
+    cout << "ATM balance: " << atm.getBalance() << endl<< "\n";
+
+    atm.insertCard();
+    atm.insertPinAndWithdrawCash(1234, 900);
+
+    cout << "ATM balance: " << atm.getBalance() << endl<< "\n";
 
     return 0;
 }
