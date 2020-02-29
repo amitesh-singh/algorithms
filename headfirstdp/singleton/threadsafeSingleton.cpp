9333ecf6f07294e1a1755ed48ad45a71ca24122b
Amitesh Singh <singh.amitesh@gmail.com>
2020-02-29 08:13:59 +0530

    Add thread safe singleton pattern

---
+++ headfirstdp/singleton/threadsafeSingleton.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <mutex> //for call_once and once_flag
+#include <thread>
+
+namespace threadsafe
+{
+    class Singleton
+    {
+        static Singleton *_instance;
+        static std::once_flag _flag;
+
+        public:
+        static Singleton *getInstance()
+        {
+            std::call_once(_flag, [&]() { _instance = new Singleton();});
+
+            return _instance;
+        }
+    };
+}
+
+threadsafe::Singleton *threadsafe::Singleton::_instance = nullptr;
+std::once_flag threadsafe::Singleton::_flag;
+
+std::mutex mu;
+void func(std::string s)
+{
+    std::lock_guard<std::mutex> locker(mu);
+    std::cout << s << " : " << threadsafe::Singleton::getInstance() << std::endl;
+}
+
+int main()
+{
+    std::thread t1(func, "thread 1");    
+    std::thread t2(func, "thread 2");
+    
+    t1.join();
+    t2.join();
+
+    func("main thread");
+    return 0;
+}
