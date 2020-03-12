f946a22a860da73e9f0308982928740ae6c5825b
Amitesh Singh <singh.amitesh@gmail.com>
2020-03-12 21:30:37 +0530

    clean up code.

--- lang/cpp/stl/algorithm/sort.cpp
+++ lang/cpp/stl/algorithm/sort.cpp
@@ -1,56 +1,43 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <iterator> //for ostream_iterator
 
 using namespace std;
 
-static void
-printArr(int arr[], int len)
-{
-   for (int i = 0; i < len; ++i)
-     {
-        cout << arr[i] << " ";
-     }
-   cout << endl;
-}
-
-#include <vector>
 int main()
 {
    //sorting arrays 
-   int arr[10] = { 9, 4, 34, 4200, 100, 324,
-        87, 98, 87, 422};
+   int arr[10] = { 9, 4, 34, 4200, 100, 324, 87, 98, 87, 422};
 
-   printArr(arr, 10);
+   std::copy(arr, arr + sizeof(arr)/sizeof *arr, std::ostream_iterator<int>(std::cout, ", "));
+   std::cout << std::endl;
 
    cout << "Sorting arry via std::sort() ---->\n";
    std::sort(arr, arr + sizeof(arr)/sizeof(arr[0]));
 
-   printArr(arr, 10);
+   std::copy(arr, arr + sizeof(arr)/sizeof *arr, std::ostream_iterator<int>(std::cout, ", "));
+   std::cout << std::endl;
+
 
    //sorting vector
    std::vector<int> v;
+
+   srand(time(nullptr));
 
    for (int i = 9; i >= 0; --i)
      {
         v.push_back(rand() % 2398);
      }
 
-   for (int i = 0; i < v.size(); ++i)
-     {
-        cout << v[i] << " ";
-     }
-   cout << endl;
+   std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+   std::cout << std::endl;
 
    std::sort(v.begin(), v.end());
-   for (int i = 0; i < v.size(); ++i)
-     {
-        cout << v[i] << " ";
-     }
+
+   std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ", "));
+   std::cout << std::endl;
+
    v.clear();
-
-   for (int i = 9; i >= 0; --i)
-     {
-     }
-
    return 0;
 }
