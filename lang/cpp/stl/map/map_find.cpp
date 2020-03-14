acb1a5b7dae509c3bd17d965fbbdccaf77744dc0
Amitesh Singh <singh.amitesh@gmail.com>
2020-03-14 19:51:43 +0530

    indent++

--- lang/cpp/stl/map/map_find.cpp
+++ lang/cpp/stl/map/map_find.cpp
@@ -5,20 +5,20 @@ using namespace std;
 
 int main()
 {
-	map<int, char> map_key;
-    /*{ {1, 'a'}, {2, 'b'}}; This is not allowed in C++98*/
-    
-    //C++ 98 way to initialize
-    map_key[1] = 'a';
-    map_key[2] = 'b';
+   map<int, char> map_key;
+   /*{ {1, 'a'}, {2, 'b'}}; This is not allowed in C++98*/
 
-	//Lets find 2
-	map<int, char>::iterator itr_2 = map_key.find(2);
+   //C++ 98 way to initialize
+   map_key[1] = 'a';
+   map_key[2] = 'b';
 
-	//if it is not equal to end.. we got it. :)
-	if (itr_2 != map_key.end())
-	{
-		cout << "itr2 " << itr_2->first << " : " << itr_2->second << endl;
-	}
-	return 0;
+   //Lets find 2
+   map<int, char>::iterator itr_2 = map_key.find(2);
+
+   //if it is not equal to end.. we got it. :)
+   if (itr_2 != map_key.end())
+     {
+        cout << "itr2 " << itr_2->first << " : " << itr_2->second << endl;
+     }
+   return 0;
 }
