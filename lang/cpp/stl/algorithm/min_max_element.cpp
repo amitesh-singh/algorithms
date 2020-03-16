9bc3f2529b49be1158f3cc329b1c0649691a730c
Amitesh Singh <singh.amitesh@gmail.com>
2020-03-16 21:17:33 +0530

    min max element exaample

---
+++ lang/cpp/stl/algorithm/min_max_element.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <list>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> v {10, 220, 23, 45, 450, -1};
+    auto minItr = std::min_element(begin(v), end(v));
+    auto maxItr = std::max_element(begin(v), end(v));
+
+    std::cout << "min element is: " << *minItr << std::endl;
+    std::cout << "max element is: " << *maxItr << std::endl;
+    int pos = minItr - v.begin();
+
+    //minmax type is pair<std::vector<int>::iterator, std::vector<int>::iterator> >
+    auto minmax = std::minmax_element(v.begin(), v.end());
+    std::cout << "pair<int, int> minmax = " << *minmax.first << ", "
+             << *minmax.second << std::endl;
+    int minPos = minmax.first - v.begin();
+    int maxPos = minmax.second - v.begin();
+
+    std::pair<int, int> minmaxPair;
+    minmaxPair = std::minmax({10, 23, 343, 344, 1});
+    std::cout << minmaxPair.first << ", " << minmaxPair.second << std::endl;
+
+    std::cout << "min(10, 20): " << std::min(10, 20) << std::endl;
+    std::cout << "max(20, 34): " << std::max(20, 34) << std::endl;
+
+    return 0;
+}
