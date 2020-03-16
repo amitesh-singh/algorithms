46ba1f00837017b7972d3ef7778c37e24de8b9ad
Amitesh Singh <singh.amitesh@gmail.com>
2020-03-16 22:01:13 +0530

    implement using heapify

--- lang/cpp/stl/queue/priority_queue.cpp
+++ lang/cpp/stl/queue/priority_queue.cpp
@@ -134,6 +134,98 @@ namespace nonstd
          return _size;
       }
    };
+
+    template<class T, int fixed_size = 5>
+   class fixed_size_arr_pqueue_v2
+   {
+      std::array<T, fixed_size> _data;
+      int _size = 0;
+
+      int parent(int i)
+      {
+         return (i - 1)/2;
+      }
+
+      void heapify(int i, bool downward = false)
+      {
+         int l = 2*i + 1;
+         int r = 2*i + 2;
+         int largest = 0;
+         if (l < size() && _data[l] > _data[i])
+            largest = l;
+         else
+            largest = i;
+
+         if (r < size() && _data[r] > _data[largest])
+            largest = r;   
+
+         if (largest != i)
+         {
+            std::swap(_data[largest], _data[i]);
+            if (!downward)
+               heapify(parent(i));
+            else
+               heapify(largest);
+         }
+      }
+
+      public:
+      /*
+      void print()
+      {
+         for (auto &x: _data)
+            std::cout << x << " , ";
+         std::cout << std::endl;
+      }
+      */
+
+      void push(T &d)
+      {
+         if (_size == fixed_size)
+         {
+            //min elements in a max heap lies at leaves only. 
+            auto minItr = std::min_element(begin(_data) + _size/2, end(_data));
+            auto minPos {minItr - _data.begin()};
+            auto min { *minItr};
+            
+            if (d > min)
+            {
+               _data.at(minPos) = d;
+               if (_data[parent(minPos)] > d)
+               {
+                  //this is unlikely to happen in our case? as this position is gonna be a leaf?
+                  heapify(minPos, true);
+               }
+               else
+                  heapify(parent(minPos));
+               //std::make_heap(begin(_data), end(_data));
+            }           
+
+            return ;
+         }
+
+         _data.at(_size++) = d;
+         std::push_heap(_data.begin(), _data.begin() + _size);
+      }
+
+      T pop()
+      {
+         T d = _data.front();
+         std::pop_heap(_data.begin(), _data.begin() + _size);
+         _size--;
+         return d;
+      }
+
+      T top()
+      {
+         return _data.front();
+      }
+
+      int size() const
+      {
+         return _size;
+      }
+   };
 }
 
 void f3()
@@ -171,12 +263,31 @@ void f4()
    }
 }
 
+void f5()
+{
+   std::cout << "fixed size priority_queue v2 using std::array: \n";
+   nonstd::fixed_size_arr_pqueue_v2<int, 5> pq;
+   std::vector<int> v {90, 23, 99, 23, -9, 0, 23, 45, 41, 20};
+
+   for (int i = 0; i < v.size(); ++i)
+      pq.push(v[i]);
+
+   int pqSize = pq.size();
+   std::cout << "size: " << pq.size() << std::endl;
+   for (int i = 0; i < pqSize; ++i)
+   {
+         std::cout << pq.top() << std::endl;
+         pq.pop();
+   }
+}
+
 int main()
 {
    f1();
    f2();
    f3();
    f4();
+   f5();
 
    return 0;
 }
