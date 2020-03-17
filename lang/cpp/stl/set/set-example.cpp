f3c9ea94b1fe02a46edbb3c24ae391041e063d54
Amitesh Singh <singh.amitesh@gmail.com>
2020-03-17 19:56:15 +0530

    make set example better

--- lang/cpp/stl/set/set-example.cpp
+++ lang/cpp/stl/set/set-example.cpp
@@ -2,31 +2,32 @@
 #include<set>
 using namespace std;
 
-template<class T>
-class len_order
-{
- public:
-    bool operator()(T a,T b) const
-      {
-         return a.id <b.id;
-      }
-
-};
 
 class client
 {
  public:
     int id;
+    client(int i):id (i) {}
 };
+
+struct clientcomp
+{
+   bool operator()(const client &lhs, const client &rhs)
+     {
+        return lhs.id < rhs.id;
+     }
+};
+
+void print(std::set<int> &s)
+{
+   for (auto &x: s)
+     std::cout << x << ", ";
+   std::cout << std::endl;
+}
 
 int main()
 {
-   //note:
-   //by default: its std::less<T> 
-   // e.g. set<int, std::less<int> > s;
-   // or set<int> s; both statements are same.
-   //now S is in decending order.
-   std::set<int,std::greater<int> > S;
+   std::set<int, std::greater<int>> S;
 
    S.insert(1);
    S.insert(2);
@@ -34,7 +35,58 @@ int main()
    S.insert(0);
    for(std::set<int>::iterator itr = S.begin(); itr!=S.end(); ++itr)
      cout << *itr << endl;
-   std::set<client,len_order<client> > s1;
+
+   std::set<client, clientcomp> s1;
+   s1.insert(client(10));
+   s1.insert(client(-1));
+   s1.insert(client(0));
+   s1.insert(client(9));
+
+   for (auto &x: s1)
+     std::cout << x.id << ", ";
+   std::cout << std::endl;
+
+     {
+        std::set<int> s;
+        for (int i = 0; i < 5; ++i)
+          s.insert(i*10);
+
+        print(s);
+
+        std::cout << "inserting 99 at the head\n";
+        auto itr = s.begin();
+        //insert at the begining.
+        s.insert(itr, 99);
+        print(s);
+
+        //remember in set, we can only have unique elements.
+        int arr[] {23, 34, 56, 56};
+        s.insert(arr, arr + 4);
+        print(s);
+
+        //erasing 23
+        std::cout << "deleting 23\n";
+        s.erase(23);
+        print(s);
+
+        s.empty() ? std::cout << "set is empty\n" : std::cout << "set is not empty\n";
+        std::cout << "size = " << s.size() << " max_size = " << s.max_size() << std::endl;
+
+        //find 56
+        itr = s.find(56);
+        if (itr != s.end())
+          std::cout << "found " << *itr << std::endl;
+
+        //how many times 56 is present in the set?
+        int rep = s.count(56);
+        std::cout << "56 is present " << rep << " times.\n";
+
+        std::cout << "clearing the set\n";
+        s.clear();
+
+        s.empty() ? std::cout << "set is empty\n" : std::cout << "set is not empty\n";
+     }
+
 
    return 0;
 }
