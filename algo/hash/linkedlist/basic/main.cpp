f44c1b49d70872fed617dccd7d1f86798813ed26
Amitesh Singh <singh.amitesh@gmail.com>
2021-03-03 18:11:41 +0530

    improve it

--- algo/hash/linkedlist/basic/main.cpp
+++ algo/hash/linkedlist/basic/main.cpp
@@ -8,8 +8,9 @@ struct node
 {
     int data;
     node *next;
+    node *prev;
 
-    node(int _d):data(_d), next(nullptr) {}
+    node(int _d):data(_d), next(nullptr), prev(nullptr) {}
 };
 
 node *chain[HASH_SIZE];
@@ -38,6 +39,7 @@ void insert(int val)
         }
 
         tmp->next = newnode;
+        tmp->next->prev = tmp;
     }
 }
 
@@ -61,16 +63,72 @@ void print()
     }    
 }
 
+bool search(int val)
+{
+    unsigned int key = val % HASH_SIZE;
+
+    if (chain[key])
+    {
+        node *tmp = chain[key];
+        while (tmp)
+        {
+            if (tmp->data == val)
+                return true;
+            tmp = tmp->next;
+        }
+    }
+
+    return false;
+}
+
+void del(int val)
+{
+    unsigned int key = val % HASH_SIZE;
+    node *tmp = chain[key];
+    while (tmp)
+    {
+        if (tmp->data == val)
+        {
+            cout << "found\n";
+            break;
+        }
+
+        tmp = tmp->next;
+
+    }
+    node *tmp2 = tmp;
+    if (tmp->prev)
+        tmp->prev->next = tmp->next;
+    if (tmp->next)
+        tmp->next->prev = tmp->prev;
+    if(!tmp->prev && !tmp->next) chain[key] = nullptr;
+    delete tmp2;
+    
+}
+
 int main()
 {
     init();
+
     insert(100);
     insert(200);
     insert(1000);
     insert(1);
     insert(0);
     insert(1001);
-    
+
     print();
+
+    cout << "searching for 1001" << (search(1001) ? "found" : "not found") << endl;
+
+    del(100);
+
+    print();
+
+    del(1001);
+
+    print();
+
+    
     return 0;
 }
