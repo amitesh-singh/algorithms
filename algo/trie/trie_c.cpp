dda647120a7bd3ffc4717afe8f5430365b1d14ca
Amitesh Singh <singh.amitesh@gmail.com>
2021-03-26 21:28:10 +0530

    fix trie again

--- algo/trie/trie_c.cpp
+++ algo/trie/trie_c.cpp
@@ -40,9 +40,10 @@ void insert(node *&root, const char *key)
    node *tmp = root;
    for (int i = 0; i < n; ++i)
      {
-        if (!tmp->children[i])
-          tmp->children[i] = new node(key[i], false);
-        tmp = tmp->children[i];
+        int index = key[i] - 'a';
+        if (!tmp->children[index])
+          tmp->children[index] = new node(key[i], false);
+        tmp = tmp->children[index];
      }
 
    tmp->isLast = true;
@@ -57,12 +58,61 @@ bool search(node *&root, const char *key)
 
    for (int i = 0; i < n; ++i)
      {
-        if (!tmp->children[i])
+        int index = key[i] - 'a';
+        if (!tmp->children[index])
           return false;
-        tmp = tmp->children[i];
+        tmp = tmp->children[index];
      }
 
    return (tmp && tmp->isLast);
+}
+
+
+
+bool remove(node *&root, const char *key)
+{
+   auto hasChildren = [](node *root)->bool {
+        if (root == nullptr) return false;
+        for (int i = 0; i < ALPHA_SIZE; ++i)
+          {
+             if (root->children[i])
+               return true;
+          }
+        return false;
+   };
+   if (!root) return false;
+   if (*key)
+     {
+        int index = key[0] - 'a';
+        if (root && root->children[index] &&
+            remove(root->children[index], key + 1) &&
+            !root->isLast)
+          {
+             if (!hasChildren(root))
+               {
+                  delete root;
+                  root = nullptr;
+                  return true;
+               }
+             else
+               return false;
+          }
+     }
+   if (*key == '\0' && root->isLast)
+     {
+        if (!hasChildren(root))
+          {
+             delete root;
+             root = nullptr;
+             return true;
+          }
+        else
+          {
+             root->isLast = false;
+             return false;
+          }
+     }
+   return false;
 }
 
 int main()
