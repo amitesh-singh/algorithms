5af62a8abd6ca0b65ceb8e2571b079d8cbd7b5f1
Amitesh Singh <singh.amitesh@gmail.com>
2021-03-30 18:57:50 +0530

    Add global memtest

---
+++ algo/placementnew/memtest.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+char mempool[101];
+int memIndex = 0;
+
+int main()
+{
+    int *p1 = new (mempool + memIndex) int;
+    memIndex += sizeof(int);
+    *p1 = 1;
+
+    int *p2 = new (mempool + memIndex) int;
+    memIndex += sizeof(int);
+    *p2 = 2;
+
+    char *c = new (mempool + memIndex) char;
+    memIndex += sizeof(char);
+    *c = 'A';
+
+    cout << "mem index: " << memIndex << endl;
+
+    //print the mempool
+    cout << std::hex;
+    for (int i = 0; i < memIndex; ++i)
+    {
+        cout << std::hex << int(mempool[i]) << " ";
+    }
+    cout << "\n";
+
+    return 0;
+}
