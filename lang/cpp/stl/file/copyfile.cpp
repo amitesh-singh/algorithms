5330208a689f4b113092fca530cd3eacf2fe9ff6
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-06 21:01:18 +0530

    add an example of file copy

---
+++ lang/cpp/stl/file/copyfile.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        cerr << argv[0] << " <file> <copiedfile>\n";
+        return -1; 
+    }
+
+    string file(argv[1]), cfile(argv[2]);
+
+    ifstream fin(file);
+    if (!fin)
+    {
+        cerr << "Error in reading " << file << endl;
+        return -2;
+    }
+    ofstream fout(cfile);
+    string s;
+
+    while(getline(fin, s))
+    {
+        fout << s << "\n";
+    }
+
+    return 0;
+}
