5508d49cb045773c8e2b6e08a3fbfe49285a0a85
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-07 15:59:24 +0530

    add

--- lang/cpp/stl/map/ex1.cpp
+++ lang/cpp/stl/map/ex1.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
+
 using namespace std;
 
 
 int main()
 {
+   //self balancing BST like red black tree is map
    map<int, int> m;
+
+   std::unordered_map<int, int> m2;
 
    m[1] = 10;
    m[22] = 34;
    m[2] = 20;
+
+   m2[1] = 10;
+   m2[22] = 34;
+   m2[2] = 20;
 
    map<int, int>::iterator itr = m.begin();
 
@@ -25,5 +34,9 @@ int main()
         cout << x.first << " : " << x.second << endl;
      }
 
+   cout << "unordered_map: \n";
+   for (auto &x: m2)
+      cout << x.first << ": " << x.second << endl;
+ 
    return 0;
 }
