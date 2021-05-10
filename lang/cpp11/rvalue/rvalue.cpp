04313093e06300223c2b417c9ab2f2c898495e74
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-10 22:48:50 +0530

    add more doc

--- lang/cpp11/rvalue/rvalue.cpp
+++ lang/cpp11/rvalue/rvalue.cpp
@@ -8,16 +8,28 @@ using namespace std;
     ordinary reference x& is now called lvalue reference.
 */
 
+/*
+ 
+   //this can only call lvalue
+   void foo(int &x)
+   {
+   }
+
+foo(const int &x) and foo(int &x) should not be declared together
+*/
+
 //can call  lvalue or rvalue
 void foo(const int &x)
 {
     F;
 }
 
+//can only call rvalue
 void foo(int &&x)
 {
     F;
 }
+
 
 int foobar()
 {
@@ -32,20 +44,27 @@ int &foobar2()
 
 int main()
 {
-    foo(1); //calls foo(int&&)
-    foo(foobar()); //calls foo(&&)
-    int x {1};
-    foo(x); //calls foo(const int&)
+   //int &x = 10; will not compile
+   // can bind a lvalue to a rvalue
+   const int &a1 = 10; //OK
 
-    foo({1}); //since {1} is rval, foo(&&)
-    
-    foo(std::move(x)); //calls foo(&&)
+   //foo(int &&) will given preference over foo(const int &)
+   foo(1); //calls foo(int&&)
+   foo(foobar()); //calls foo(&&)
+   int x {1};
+   foo(x); //calls foo(const int&)
 
-    foo(foobar2()); //calls foo(&), since foobar2() is lvalue, foobar2() = 10;
-    const int a {1};
-    foo(a); //call foo(const int&)
+   foo({1}); //since {1} is rval, foo(&&)
 
-    foo(1 + 3);
-    
-    return 0;
+   foo(std::move(x)); //calls foo(&&)
+
+   foo(foobar2()); //calls foo(&), since foobar2() is lvalue, foobar2() = 10;
+   const int a {1};
+   foo(a); //call foo(const int&)
+
+   foo(1 + 3); //calls foo(int &&);
+   //std::move() makes a lvalue to a rvalue
+   foo(std::move(x));
+
+   return 0;
 }
