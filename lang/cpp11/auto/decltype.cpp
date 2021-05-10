ae577ab776527a818599b78633976f3c461b8048
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-10 23:06:42 +0530

    make code better

--- lang/cpp11/auto/decltype.cpp
+++ lang/cpp11/auto/decltype.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+struct bar
+{
+   bar(int) {} //implicitly constructable
+};
+
+struct bar2
+{
+   bar2(int) {} //implicitly constructable
+};
+
+struct foo
+{
+   static bar var;
+};
+
+struct foo2
+{
+   static bar2 var;
+};
+
+template<class T>
+void dummy()
+{
+   decltype(T::var) meh = 43;
+   if (std::is_same<decltype(T::var), bar>::value)
+     std::cout << "type is bar\n";
+   else if (std::is_same<decltype(T::var), bar2>::value)
+     std::cout << "type is bar2\n";
+}
+
 // The decltype can be used to determine the type of an expression at compile-type.
 int main()
 {
@@ -11,6 +41,10 @@ int main()
    auto z = b; // z would be int
 
    cout << z << endl;
+
+   dummy<foo>(); //meh is of type bar
+   dummy<foo2>(); //meh is of type bar2
+
 
    return 0;
 }
