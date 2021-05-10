b4694020e3823c4602139633b3b8d225b0316dd7
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-10 21:07:38 +0530

    Add a complete exxample

--- lang/cpp17/if-switch/main.cpp
+++ lang/cpp17/if-switch/main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+int randfunc()
+{
+  return rand();
+}
+
 int main()
 {
+  srand(time(nullptr));
+       //init-statment; condition
    if (int i = 10; i == 10)
      cout << "it's equal to " << i << endl;
+   
+   if (int r = randfunc(); r % 2 == 0)
+    cout << "even\n";
+   else if (int r = randfunc(); r % 2 == 1)
+    cout << "odd\n";
 
+   //switch
+
+   switch(int i = rand(); i % 2)
+   {
+     case 0: 
+      cout << "switch: even\n";
+      break;
+     case 1:
+      cout << "switch: odd\n";
+      break;
+     default:
+      cout << "default:\n";
+   }
    return 0;
 }
