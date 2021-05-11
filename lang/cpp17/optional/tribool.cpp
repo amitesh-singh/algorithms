b5105b004c2fe0774aa444965fd8b50f139a5355
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-11 20:44:02 +0530

    add tribool example in cpp

--- lang/cpp17/optional/tribool.cpp
+++ lang/cpp17/optional/tribool.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 #include <optional>
 
+#define F std::cout << __PRETTY_FUNCTION__ << std::endl
+namespace nonstd
+{
+    struct tribool
+    {
+        private:
+        std::optional<bool> _state;
+        public:
+        tribool &operator=(std::optional<bool> &&rhs)
+        {
+            F;
+            _state.emplace(*rhs);
+            return *this;
+        }
+
+        tribool &operator=(bool &&rhs)
+        {
+            F;
+            _state.emplace(rhs);
+            return *this;
+        }
+
+        bool operator==(bool &&rhs)
+        {
+            F;
+            return (rhs == *_state);
+        }
+
+        operator bool() const 
+        {
+            F;
+            if (_state)
+                return *_state;
+            else return false;
+        }
+    };
+}
+
 int main()
 {
-    std::optional<bool> tribool1;
-   // *tribool1 = true;
-    tribool1.emplace(true);
-    
-    std::cout << tribool1.value() << std::endl;
-
+    nonstd::tribool tristate;
+    tristate = true;
+    if (tristate)
+    {
+        std::cout << "it's true\n";
+    }
+    tristate = std::nullopt;
+    if (tristate)
+    {
+        std::cout << "should not come here\n";
+    }
     return 0;
 }
