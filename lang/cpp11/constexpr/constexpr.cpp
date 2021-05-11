7bb9ac77bef93f934137a6ea5ab6fccebd8fda61
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-11 11:08:01 +0530

    add comments

--- lang/cpp11/constexpr/constexpr.cpp
+++ lang/cpp11/constexpr/constexpr.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 #define GPIOC 1
 #define GPIO13 1 << 13
+/*
+ difference between const and constexpr
+ const double pi1 = 3.14;
+ constexpr double pi2 = 3.14;
+ both pi1 & pi2 are constants, meaning you can't modify them. However pi2
+ is a compile-time constant. It shall be initialized at compile time.
+ pi1 may be initialized at compile time or run time, depends upon compiler optimization.
+ Furthermore, only pi2 can be used in a context that requires a compile time constant.
 
+*/
 class Pin
 {
 public:
@@ -26,7 +35,7 @@ int main()
 
   cout << pin._ddr << ":" << pin._port << std::endl;
 
- constexpr int len = 10;
+  constexpr int len = 10;
   constexpr int len_2 = len + 1;
   
   cout << PI() << endl;
