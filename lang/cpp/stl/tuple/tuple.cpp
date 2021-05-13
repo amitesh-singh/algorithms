4180f28e6e3ee588d99801d9b962ed9ff039cc9e
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-13 22:16:48 +0530

    add tuple code

--- lang/cpp/stl/tuple/tuple.cpp
+++ lang/cpp/stl/tuple/tuple.cpp
@@ -1,11 +1,33 @@
 #include <tuple>
 #include <iostream>
+#include <optional>
 
 int main()
 {
     std::tuple<int, std::string, int, float> t {1, "ami", 20, 20.2};
-    int firstarg = std::get<0>(t)
+    int firstarg = std::get<0>(t);
+    std::string &secondarg = std::get<1>(t);
+
     std::cout << firstarg << std::endl;
-    
+    std::cout << secondarg << std::endl;
+    //change the value
+    std::get<3>(t) = 30.2;
+
+    std::cout << std::get<3>(t) << std::endl;
+
+    //use tie
+    int i, j;
+    float x;
+    std::string name;
+    std::tie(i, name, j, x) = t;
+
+    //c++17 way
+    //structured binding
+    auto [id, fullname, age, marks] = t;
+
+    std::tuple<int, std::string, std::optional<std::string>> person;
+
+    person = std::make_tuple(20, "Ami", std::nullopt);
+
     return 0;
 }
