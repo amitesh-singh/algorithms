60ce2692aa8a9f29fb88f67dd07d5e83b41615b4
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-15 01:41:27 +0530

    Add code for tree

--- algo/tree/cormen/newmain.cpp
+++ algo/tree/cormen/newmain.cpp
@@ -2,6 +2,24 @@
 
 #include <ctime>
 
+struct mytree: public myds::tree<int>
+{
+   void _liftTraverse(node *&p, int k)
+     {
+        if (p == nullptr || k == 0)
+          {
+             return;
+          }
+        _liftTraverse(p->parent, --k);
+        std::cout << p->data << std::endl;
+     }
+
+   void uptraverse(node *p, int k)
+     {
+        _liftTraverse(p, k);
+     }
+};
+
 int main()
 {
    srand(time(nullptr));
@@ -10,7 +28,28 @@ int main()
    t.insert(10);
    t.insert(11);
    t.insert(12);
+   for (int i = 0; i < 25; ++i)
+     {
+        t.insert(i);
+     }
+
+   myds::tree<int>::node *ptr = t.search(11);
+   std::cout << "found " << ptr->data  << " parent: " << ptr->parent->data << std::endl;
 
    t.inorder();
+
+   mytree mt;
+   mt.insert(10);
+   mt.insert(9);
+   mt.insert(11);
+   mt.insert(12);
+   mt.insert(13);
+
+   using treenode = myds::tree<int>::node;
+   treenode *p = mt.search(13);
+
+   std::cout << "up traverse: \n";
+   mt.uptraverse(p->parent, 10);
+
    return 0;
 }
--- algo/tree/cormen/tree.h
+++ algo/tree/cormen/tree.h
@@ -7,25 +7,29 @@ namespace myds
    template<class T>
        class tree
          {
+           public:
             struct node
               {
                  node *left, *right;
                  T data;
-                 node(const T &d): data(d), left(nullptr), right(nullptr) {}
+                 node *parent;
+                 node(const T &d): data(d), left(nullptr), right(nullptr), parent(nullptr) {}
               };
 
+           protected:
             node *root;
-            void _insert(node *&p,T d)
+            void _insert(node *&p,T d, node *par = nullptr)
               {
                  if (p == nullptr)
                    {
                       p = new node(d);
+                      p->parent = par;
                       return;
                    }
                  if (d < p->data)
-                   _insert(p->left, d);
+                   _insert(p->left, d, p);
                  else if (d > p->data)
-                   _insert(p->right, d);
+                   _insert(p->right, d, p);
               }
 
             void _inorder(node *p)
@@ -35,8 +39,50 @@ namespace myds
                  std::cout << p->data << std::endl;
                  _inorder(p->right);
               }
+
+            void _preorder(node *p)
+              {
+                 if (p == nullptr) return;
+                 std::cout << p->data << std::endl;
+                 _preorder(p->left);
+                 _preorder(p->right);
+              }
+
+            void _postorder(node *p)
+              {
+                 if (p == nullptr) return;
+                 _postorder(p->left);
+                 _postorder(p->right);
+                 std::cout << p->data << std::endl;
+              }
+
+           node *_search(node *p, const T &d)
+             {
+                if (p == nullptr)
+                  return nullptr;
+                if (d == p->data)
+                  return p;
+                else if (d < p->data)
+                  return _search(p->left, d);
+                else if (d > p->data)
+                  return _search(p->right, d);
+                return nullptr;
+             }
+
+           void _deleteTree(node *&p)
+             {
+                if (!p) return;
+                _deleteTree(p->left);
+                _deleteTree(p->right);
+                delete p;
+                p = nullptr;
+             }
            public:
             tree(): root(nullptr) {}
+            ~tree()
+              {
+                 _deleteTree(root);
+              }
             tree(const std::initializer_list<int> &l): root (nullptr)
               {
                  for (auto &x: l)
@@ -50,6 +96,20 @@ namespace myds
             void inorder()
               {
                  _inorder(root);
+              }
+
+            void preorder()
+              {
+                 _preorder(root);
+              }
+            void postorder()
+              {
+                 _postorder(root);
+              }
+
+            node *search(const T &d)
+              {
+                 return _search(root, d);
               }
          };
 }
