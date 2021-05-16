9bb601ab185235a69edf62ba861d1ea6129c21a4
Amitesh Singh <singh.amitesh@gmail.com>
2021-05-16 12:01:35 +0530

    add

--- algo/tree/final/issame.cpp
+++ algo/tree/final/issame.cpp
@@ -36,18 +36,17 @@ int main()
 
    std::cout << "equal check: " << issame(t, t2) << std::endl;
    std::cout << "native code: " << (t == t2) << std::endl;
-
    using fptr = bool (*)(node *, node *);
+
    fptr f = issame;
    //can we do better and use thread for checking left and right subtree simultaneously? possible
-   auto leftSubtreeWorker = std::async(std::launch::async, f, t.root->left, t2.root->left);
+   auto leftSubtreeWorker = std::async(std::launch::async, (bool (*)(node *, node *)) (issame), t.root->left, t2.root->left);
    auto rightSubtreeWorker = std::async(std::launch::async, f, t.root->right, t2.root->right);
 
    std::cout << "waiting for thread to do the job " << std::endl;
    auto l = leftSubtreeWorker.get();
    auto r  = rightSubtreeWorker.get();
    std::cout << "threaded check: " << ((t.root->data == t2.root->data) && l && r) << std::endl;
-
 
    return 0;
 }
