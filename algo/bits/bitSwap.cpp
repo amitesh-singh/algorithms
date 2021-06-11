798697a1d67c701f190a0248e8096dce72957d74
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-11 18:02:14 +0530

    Add bit swap

---
+++ algo/bits/bitSwap.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+//A to B
+//
+
+int countBitsSwap(int a, int b)
+{
+   int count {0};
+
+   for (int c = a^b; c > 0; c = (c & (c-1)))
+     {
+        count += (c & 1);
+     }
+
+   return count;
+}
+int main()
+{
+   std::cout << countBitsSwap(1, 0) << std::endl;
+   return 0;
+}
