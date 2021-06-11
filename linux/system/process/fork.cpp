ef70e1ba1b9ec22e5b4fb2b5dacbb717265a54d4
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-11 21:24:36 +0530

    fork

--- linux/system/process/fork.cpp
+++ linux/system/process/fork.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
 int main()
 {
@@ -12,13 +13,18 @@ int main()
 
    if (child_id != 0)
      {
+        //parent
         std::cout << "This is parent: " << getpid() << std::endl;
         std::cout << "child id in parent is: " << child_id << std::endl;
+        std::cout << "waiting for the child to exit\n";
+        waitpid(child_id, nullptr, 0);
      }
    else
      {
+        //child
         //this is child since return value of fork() is zero
         std::cout << "This is child: " << getpid() << std::endl;
+        exit(0);
      }
    return 0;
 }
