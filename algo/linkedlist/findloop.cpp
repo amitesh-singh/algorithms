906c6b564de3e17635ac0e87b9e0c751f7e9b5f7
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-13 18:29:39 +0530

    Add new code

--- algo/linkedlist/findloop.cpp
+++ algo/linkedlist/findloop.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <unordered_set>
+#include <ctime>
+#include <unordered_map>
 
 template<typename T>
 struct ListNode
@@ -33,24 +35,79 @@ bool hasLoop(node *head, node *&loop_node)
    return false;
 }
 
+// find loop in O(1)
+// 
+bool hasLoop2(node *head, node *&loop_node)
+{
+   node *p = head;
+   bool isLoop = false;
+   //first detect if there is a loop
+   node *slow = head, *fast = head;
+
+   while (slow && fast && fast->next)
+     {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast)
+          {
+             isLoop = true;
+             break;
+          }
+     }
+   if (!isLoop)
+     {
+        loop_node = nullptr;
+        return false;
+     }
+
+   // find the loop node
+   // reset the slow pointer to head and move both fast and slow one step
+   slow = head;
+   while (slow && fast)
+     {
+        slow = slow->next;
+        fast = fast->next;
+        if (slow == fast)
+          {
+             loop_node = slow;
+             break;
+          }
+     }
+
+   return true;
+}
+
 int main()
 {
+   std::srand(std::time(nullptr));
    node *head = nullptr;
 
    head = new node(0);
    node *tmp = head;
    node *oldnode;
+   std::unordered_map<int,node*> hash;
+   hash[head->val] = head;
+   int count = 1;
    for (int i = 1; i < 10; ++i)
      {
         tmp->next = new node(std::move(i));
-        if (i == 5)
-          oldnode = tmp->next;
+        hash[tmp->next->val] = tmp->next;
         tmp = tmp->next;
+        count++;
      }
-   tmp->next = oldnode;
+   tmp->next = hash[std::rand() % count];
 
+   std::cout << '\n';
    node *loop_node = nullptr;
    bool foundLoop = hasLoop(head, loop_node);
+   if (foundLoop)
+     {
+        std::cout << "found loop at " << loop_node->val << std::endl;
+     }
+
+   loop_node = nullptr;
+   foundLoop = hasLoop2(head, loop_node);
+
    if (foundLoop)
      {
         std::cout << "found loop at " << loop_node->val << std::endl;
