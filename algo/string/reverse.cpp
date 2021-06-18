2df1ed29e6a38c26fd133926871a888189930e72
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-18 19:36:03 +0530

    Add reverse

---
+++ algo/string/reverse.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string reverseStr(std::string str)
+{
+    unsigned int start = 0, end = str.size() - 1;
+
+    while (start < end)
+    {
+        std::swap(str[start], str[end]);
+        ++start;
+        --end;
+    }
+
+    return str;
+}
+
+int main()
+{
+    std::cout << "reverse: CAT: " << reverseStr("CAT") << std::endl;
+    
+    return 0;
+}
