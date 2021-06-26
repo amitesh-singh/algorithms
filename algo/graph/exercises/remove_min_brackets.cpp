360b8d9a9ddacd42b421f1f0787ab8bedecfa389
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-26 21:47:09 +0530

    Add a question

---
+++ algo/graph/exercises/remove_min_brackets.cpp
@@ -0,0 +1,22 @@
+/*
+ *
+ * Given a string of parenthesis and alphabtes, digits (a(0)) or
+ * (#($)*), remove the minimum number of brackets to get a balanced bracket
+ * sequence i.e. (()()) or ((())())
+ *
+ * input: (a)())()
+ * output: [(a)()(), (a())()]
+ * */
+
+#include <iostream>
+#include <vector>
+
+void getAnswer(std::string input, std::vector<std::string> &ans)
+{
+
+}
+
+int main()
+{
+   return 0;
+}
