e227cc064bba832b0e78a20b6f314150a6af7623
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-27 23:03:26 +0530

    add comments

--- algo/graph/exercises/remove_min_brackets.cpp
+++ algo/graph/exercises/remove_min_brackets.cpp
@@ -7,79 +7,116 @@
  * input: (a)())()
  * output: [(a)()(), (a())()]
  * */
+/*
 
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+This problem requires optimum solution. 
+**/
 #include <iostream>
 #include <vector>
 #include <set>
+#include <queue>
 
-bool isValid(const std::string &input)
-{
-   int cnt = 0;
+class Solution {
+public:
+    bool isValid(const std::string &input)
+    {
+        int cnt {0};
+        for (auto &x: input)
+        {
+            if (x == '(') cnt++;
+            else if (x == ')') cnt--;
+            //handle )(
+            if (cnt < 0) return false;
+        }
+        
+        return cnt == 0;
+    }
+    
+    void generatePossibleComb(const std::string &str, std::vector<std::string> &children)
+    {
+        char x;
+        for (int i = 0; i < str.size(); ++i)
+        {
+            x = str[i];
+            if (x == '(' or x == ')')
+            {
+                std::string tmp = str.substr(0, i) + str.substr(i + 1);
+                children.push_back(tmp);
+            }
+        }
+    }
+    
+    void getPossibleComb(const std::string &input, std::set<std::string> &ans)
+    {
+        std::queue<std::string> q;
+        q.push(input);
+        int level = 0;
+        while (!q.empty())
+        {
+            int n = q.size();
+            level++;
+            //std::cout << "level =" << level << std::endl;
+            for (int  i = 0; i < n; ++i)
+            {
+                std::string tmp = q.front();
+                q.pop();
+                std::vector<std::string> children;
+                generatePossibleComb(tmp, children);
 
-   for (auto x: input)
-     {
-        if (x == '(') cnt++;
-        else if (x == ')') cnt--;
-        //to handle ")("
-        if (cnt < 0) return false;
-     }
 
-   return cnt == 0;
-}
-
-void getPossibleComb(const std::string &input, std::vector<std::string> &out, char deleteChar)
-{
-   bool isPossible = false;
-   for (int i = 0; i < input.size(); ++i)
-     {
-        if (input[i] == deleteChar)
-          {
-             std::string tmp = input;
-             tmp.erase(tmp.begin() + i);
-             out.push_back(tmp);
-          }
-     }
-}
-
-void getAnswer(const std::string &input, std::set<std::string> &ans)
-{
-   std::cout << "input: ";
-   std::cout << input << std::endl;
-   //find diff between ) and (
-   int br1_count = 0, br2_count = 0;
-   for (auto &x: input)
-     {
-        if (x == '(') br1_count++;
-        else if (x == ')') br2_count++;
-     }
-
-   int diff = std::abs(br1_count - br2_count);
-   char deleteChar = br1_count > br2_count ? '(': ')';
-
-   for (int i = 0; i < diff; ++i)
-     {
-        std::vector<std::string> level;
-
-        getPossibleComb(input, level, deleteChar);
-
-        for (std::string s: level)
-          {
-             if (isValid(s)) ans.insert(s);
-          }
-     }
-   if (diff == 0 && isValid(input))
-     ans.insert(input);
-}
+                //std::cout << "tmp: " << tmp << std::endl;
+                //std::cout << "inside\n";
+                for (auto &x: children)
+                {
+                    if (isValid(x))
+                    {
+                        ans.insert(x);
+                        return;
+                    }
+                    else
+                        q.push(x);
+                }
+            }
+        }
+    }
+    
+    std::string minRemoveToMakeValid(std::string s) {
+        std::string res = "";
+        
+        std::set<std::string> ans;
+        
+        getPossibleComb(s, ans);
+        
+        if (ans.size() > 0)
+        {
+            auto itr = ans.end();
+            itr--;
+            res = *itr;
+        }
+        else if (isValid(s))
+        {
+            res = s;
+        }
+            
+        return res;
+    }
+};
 
 int main()
 {
-   std::set<std::string> ans;
-   getAnswer("(a)())()", ans);
+   Solution sol;
+   std::string ans = sol.minRemoveToMakeValid("(a)())()");
 
-   std::cout << "[";
-   for (auto &x: ans)
-     std::cout << x << ", ";
-   std::cout << "]";
+   std::cout << ans;
+   std::cout << std::endl;
+
+   ans = sol.minRemoveToMakeValid("())()(((");
+   std::cout << ans;
+   std::cout << std::endl;
+
+   ans = sol.minRemoveToMakeValid("()))((ksg(((u((()c)(");
+   std::cout << ans;
    std::cout << std::endl;
 
    return 0;
