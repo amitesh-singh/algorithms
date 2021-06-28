23800724df9f65650723fb16cb3132b337944415
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-28 22:09:52 +0530

    Add narray tree serialize example

--- algo/tree/narraytree/serialize.cpp
+++ algo/tree/narraytree/serialize.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <ctime>
 #include <string>
+#include <queue>
 
 template<typename T>
 class node
@@ -17,8 +18,8 @@ class node
 template<int N, typename T>
 class ntree
 {
-    node<T> *root = nullptr;
     public:
+    node<T> *root = nullptr;
     ntree() {}
 
 
@@ -52,7 +53,6 @@ class ntree
 
     void _preorder(node<T> *p)
     {
-        if (p == nullptr) return;
         std::cout << p->data << ",";
         for (auto &x: p->children)
             _preorder(x);
@@ -60,25 +60,65 @@ class ntree
 
     void preorder()
     {
+       if (root == nullptr) return;
         _preorder(root);
     }
 
 
     std::string _serialize(node<T> *p)
     {
-        if (p->children.size() == 0) return std::to_string(p->data) + ",X,X,";
         std::string ret = std::to_string(p->data) + ",";
+        int i = 0;
         for (auto &x: p->children)
         {
             ret += _serialize(x);
+            i++;
+            if (i == (p->children.size()))
+              ret += "#,";
         }
 
         return ret;
     }
 
+    void tokenize(const std::string &str,std::string delim, std::queue<std::string> &q)
+      {
+         int start = 0;
+         int end = str.find(delim);
+
+         while (end != std::string::npos)
+           {
+              q.push(str.substr(start, end - start));
+
+              start = end + delim.size();
+              end = str.find(delim, start);
+           }
+
+         //since 1,2,#, ignore last space
+         //q.push(str.substr(start));
+      }
+
+    node<T> *_deserialize(std::queue<std::string> &q)
+      {
+         if (q.empty()) return nullptr;
+         if (q.front() == "#") { q.pop(); return nullptr;}
+         node<T> *root = new node<T>(std::stoi(q.front()));
+         q.pop();
+
+         for (int i = 0; i < 3; ++i)
+           {
+              node<T> *tmp = _deserialize(q);
+              if (tmp)
+                root->children.push_back(tmp);
+           }
+         return root;
+      }
+
     node<T> *deserialize(std::string str)
     {
+       std::queue<std::string> q;
+       tokenize(str, ",", q);
 
+       return _deserialize(q);
     }
 
     std::string serialize()
@@ -102,5 +142,10 @@ int main()
     std::string serial_data = tree.serialize();
     std::cout << "serial data: ";
     std::cout << serial_data << std::endl;
+
+    ntree<3, int> tree2;
+    tree2.root = tree.deserialize(serial_data);
+    tree2.preorder();
+
     return 0;
 }
