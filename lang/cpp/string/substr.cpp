66192e94bf5daef80dc8ac8747789a98db9d81ba
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-28 12:54:37 +0530

    add substr example

--- lang/cpp/string/substr.cpp
+++ lang/cpp/string/substr.cpp
@@ -2,6 +2,16 @@
 #include <string>
 
 int main()
-{
+{                  // 01234
+   std::string str = "meh! hello world";
+   std::string s1 = str.substr(1, 3);
+   std::cout << str << ": " << s1 << std::endl;
+
+   //how to get a substr after a character.
+   str = "dog:cat";
+   int pos = str.find(":");
+   s1 = str.substr(pos + 1);
+   std::cout << str << ": " << s1 << std::endl;
+   
    return 0;
 }
