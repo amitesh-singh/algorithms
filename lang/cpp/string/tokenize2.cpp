6c4e6475ccca5a182d9520306dadb4f7e4bafc23
Amitesh Singh <singh.amitesh@gmail.com>
2021-06-30 15:59:23 +0530

    Add tokenize2()

--- lang/cpp/string/tokenize2.cpp
+++ lang/cpp/string/tokenize2.cpp
@@ -4,13 +4,48 @@
 //serial data for a linked list with random pointers
 //1->2,2->4,3->1
 
-void tokenize(const std::string &str, std::string delim, std::vector<std::pair<std::string, std::string> > &ans)
+struct node
+{
+   std::string d1, d2;
+   node(std::string d1, std::string d2): d1(d1), d2(d2) {}
+   void print()
+     {
+        std::cout << d1 << "->" <<  d2 << ",";
+     }
+};
+void tokenize(const std::string &str, std::string delim1, std::string delim2, std::vector<node> &ans)
 {
     if (str == "") return;
-    
+
+    int start = 0;
+    int end = str.find(delim1);
+    while (end != std::string::npos)
+      {
+         std::string tmp(str.substr(start, end - start));
+         int pos = tmp.find(delim2, 0);
+         std::string d1 = tmp.substr(0, pos);
+         std::string d2 = tmp.substr(pos + delim2.size());
+         ans.push_back(node(d1, d2));
+
+         start = end + delim1.size();
+         end = str.find(delim1, start);
+      }
+    std::string tmp(str.substr(start));
+    int pos = tmp.find(delim2, 0);
+    std::string d1 = tmp.substr(0, pos);
+    std::string d2 = tmp.substr(pos + delim2.size());
+    ans.push_back(node(d1, d2));
 }
 
 int main()
 {
+    std::vector<node> ans;
+    tokenize("1->2,3->4,5->6", ",", "->", ans);
+    for (auto &x: ans)
+      {
+         x.print();
+      }
+    std::cout << std::endl;
+
     return 0;
 }
