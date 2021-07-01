f49230d55198bd99521da5450ba105be40001a45
Amitesh Singh <singh.amitesh@gmail.com>
2021-07-01 09:09:04 +0530

    add

--- lang/cpp/string/stringtocharptr.cpp
+++ lang/cpp/string/stringtocharptr.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <cstring>
+/*
+ *1. Use the contiguous storage of C++11
 
+std::string foo{"text"};
+auto p = &*foo.begin();
+
+Pro
+
+    Simple and short
+    Fast (only method with no copy involved)
+
+Cons
+
+    Final '\0' is not to be altered / not necessarily part of the non-const memory.
+
+ * */
 int main()
 {
    std::string s = "Hello";
--- lang/cpp/string/validateHex.cpp
+++ lang/cpp/string/validateHex.cpp
@@ -1,1 +1,23 @@
 //TODO
+// 0 to 9 and a - f and A - F
+//
+
+#include <iostream>
+
+bool isHex(const std::string &str)
+{
+   for (auto &x: str)
+     {
+        if ( (x < '0' or x > '9') and (x < 'a' or x > 'f') and (x < 'A' or x > 'F'))
+          return false;
+     }
+
+   return true;
+}
+
+int main()
+{
+   std::cout << isHex("AFCD") << std::endl;
+   std::cout << isHex("BGtg") << std::endl;
+   return 0;
+}
