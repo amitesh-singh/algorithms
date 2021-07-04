c5dedaccec70ecb461e59cb1dcacffef4ba21767
Amitesh Singh <singh.amitesh@gmail.com>
2021-07-04 11:44:53 +0530

    add segment tree code

---
+++ algo/segmenttree/rmq/rmq.cpp
@@ -0,0 +1,144 @@
+//range min query
+// find min value in an array for a range i, j
+// 10, 12, 33, -1, 0, 1
+//check notes for more details
+//
+
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+class segment_tree
+{
+   using vi = std::vector<int>;
+   int n;
+   vi st, &A;
+
+   int left(int idx)
+     {
+        return (idx << 1) + 1;
+     }
+
+   int right (int idx)
+     {
+        return (idx << 1) + 2;
+     }
+
+   int parent(int idx)
+     {
+        return (idx - 1)/2;
+     }
+
+   void build(int idx, int L, int R)
+     {
+        if (L == R)
+          {
+             st[idx] = L;
+             return;
+          }
+
+        build(left(idx), L, (L + R)/2);
+        build(right(idx), (L + R)/2 + 1, R);
+
+        int p1 = st[left(idx)], p2 = st[right(idx)];
+        st[idx] = A[p1] <= A[p2] ? p1 : p2;
+     }
+
+   // returns the index of min value in the given range
+    int rmq(int idx, int L, int R, int i, int j)
+      {
+         if (i > R or j < L) return -1;
+         if (i <= L and j >= R) return st[idx];
+
+         int p1 = rmq(left(idx), L, (L + R)/2, i, j);
+         int p2 = rmq(right(idx), (L + R)/2 + 1, R, i, j);
+
+         if (p1 == -1) return p2;
+         if (p2 == -1) return p1;
+         return A[p1] <= A[p2] ? p1 : p2;
+      }
+
+    void findPosInTree_util(int idx, int L, int R, int array_index, int &ans)
+      {
+         if (array_index > R or array_index < L) return;
+         if (array_index == L and L == R)
+           {
+              ans = idx;
+              return;
+           }
+         if (ans == -1 and array_index >= L and array_index <= (L + R)/2)
+           {
+              findPosInTree_util(left(idx), L, (L + R)/2, array_index, ans);
+           }
+         else if ( ans == -1 and array_index >= (L + R)/2 and array_index <= R)
+           {
+              findPosInTree_util(right(idx), (L + R)/2 + 1, R, array_index, ans);
+           }
+      }
+
+  public:
+    segment_tree(vi &input): n(input.size()), A(input)
+   {
+      st.reserve(4 *n);
+      st.assign(4 * n, 0);
+
+      build(0, 0, n - 1);
+   }
+
+   int rmq(int i, int j)
+     {
+        return rmq(0, 0, n - 1, i, j);
+     }
+
+   int findPosInTree(int array_index)
+     {
+        int ans = -1;
+        findPosInTree_util(0, 0, n - 1, array_index, ans);
+        return ans;
+     }
+   void update(int array_index)
+     {
+        if (array_index >= n) return;
+        int pos = findPosInTree(array_index);
+
+        pos = parent(pos);
+        while (pos != 0)
+          {
+             st[pos] = A[st[left(pos)]] <= A[st[right(pos)]] ? st[left(pos)]: st[right(pos)];
+             pos = parent(pos);
+          }
+        st[pos] = A[st[left(pos)]] <= A[st[right(pos)]] ? st[left(pos)]: st[right(pos)];
+
+     }
+
+   void print()
+     {
+        std::cout << "size: " << n << std::endl;
+        for (auto &x: st)
+          {
+             std::cout << x << ",";
+          }
+        std::cout << "\n";
+     }
+
+};
+
+
+int main()
+{
+   std::vector<int> v{18, 17, 12, 19, 15, 11, 20};
+   for (auto &x: v)
+     std::cout << x << "->";
+   std::cout << std::endl;
+   segment_tree stree(v);
+
+   std::cout << "index: rmq(0, 3): " << stree.rmq(0, 3) << std::endl;
+
+   v[0] = 1;
+   stree.update(0);
+   for (auto &x: v)
+     std::cout << x << "->";
+   std::cout << std::endl;
+
+   std::cout << "index: rmq(0, 3): " << stree.rmq(0, 3) << std::endl;
+   return 0;
+}
