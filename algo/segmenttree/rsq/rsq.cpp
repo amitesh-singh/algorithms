3f0cbd19e3dd3a378c76a65ed863989c79391d2c
Amitesh Singh <singh.amitesh@gmail.com>
2021-07-05 09:52:44 +0530

    Add RSQ code.

---
+++ algo/segmenttree/rsq/rsq.cpp
@@ -0,0 +1,79 @@
+//RSQ -> range sum query
+// return sum of elements from i, j in an array 
+
+#include <iostream>
+#include <vector>
+
+using vi = std::vector<int>;
+using lvi = std::vector<long>;
+class segment_tree
+{
+   lvi tree;
+   vi &A;
+   int n;
+
+   inline int left(int idx)
+     {
+        return (idx << 1) + 1;
+     }
+
+   inline int right(int idx)
+     {
+        return (idx << 1) + 2;
+     }
+
+   void build(int idx, int L, int R)
+     {
+        if (L == R)
+          {
+             tree[idx] = A[L];
+             return;
+          }
+
+        int mid = L + (R - L)/2;
+        build(left(idx), L, mid);
+        build(right(idx), mid + 1, R);
+
+        tree[idx] = tree[left(idx)] + tree[right(idx)];
+     }
+
+   long rsq(int idx, int L, int R, int a, int b)
+     {
+        if (a > R or b < L) return 0;
+        if (a <= L and b >= R) return tree[idx];
+
+        int mid = L + (R - L)/2;
+        return rsq(left(idx), L, mid, a, b) + rsq(right(idx), mid + 1, R, a, b);
+     }
+  public:
+   segment_tree(vi &input): A(input), n (input.size())
+   {
+      tree.reserve(4 * n);
+      tree.assign(4 *n, 0);
+
+      build(0, 0, n - 1);
+   }
+
+   long rsq(int a, int b)
+     {
+        return rsq(0, 0, n - 1, a, b);
+     }
+
+   void print()
+     {
+        for (auto &x: tree)
+          std::cout << x << ",";
+        std::cout << std::endl;
+     }
+};
+
+int main()
+{
+   vi v {98, 2, -1, 233};
+   segment_tree tree(v);
+
+   tree.print();
+
+   std::cout << tree.rsq(0, 2) << std::endl;
+   return 0;
+}
