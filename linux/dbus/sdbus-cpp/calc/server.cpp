f7d01f365c4c829fc97fa62ac1cd9d78251c3cce
Amitesh Singh <singh.amitesh@gmail.com>
2021-10-22 10:59:44 +0530

    Add proxy for get info interface

--- linux/dbus/sdbus-cpp/calc/server.cpp
+++ linux/dbus/sdbus-cpp/calc/server.cpp
@@ -9,7 +9,12 @@ int main(int argc, char **argv)
 
     // Create concatenator D-Bus object.
     const char* objectPath = "/com/amitesh/calculator";
+
+    //entry for com.amitesh.calculator interface
     calculator calc(*connection, objectPath);
+
+    //entry for com.amitesh.calcualtor.info interface
+    calc_info info(*connection, objectPath);
 
     // Run the loop on the connection.
     connection->enterEventLoop();
