23c478c9fd5cf9cbc240974cd91de5327c6be474
Amitesh Singh <singh.amitesh@gmail.com>
2021-12-24 16:09:41 +0530

    We don't need object

--- linux/dbus/sdbus-cpp/dbus-send-client/server.cpp
+++ linux/dbus/sdbus-cpp/dbus-send-client/server.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
 #include <sdbus-c++/sdbus-c++.h>
 
-
 int main()
 {
    //create Dbus connection to the system bus
    const char *serviceName = "org.sdbuscpp.add";
    auto connection = sdbus::createSystemBusConnection(serviceName);
 
-   //create dbus object
-   const char *objPath = "/org/sdbuscpp/add";
-   auto add = sdbus::createObject(*connection, objPath);
-
-   const char *intfName = "org.sdbuscpp.add";
-   add->registerSignal("addition").onInterface(intfName).withParameters<std::string>();
-   add->finishRegistration();
-
-
    //creat a proxy which triggers by dbus-send signal emit
    const char *destName = "org.sdbuscpp.ext";
-   const char *objPath2 = "/org/sdbuscpp/ext";
-   auto addProxy = sdbus::createProxy(*connection, destName, objPath2);
+   const char *objPath = "/org/sdbuscpp/ext";
+   auto proxy = sdbus::createProxy(*connection, destName, objPath);
 
-   const char *intfName2 = "org.sdbuscpp.ext";
-
-   addProxy->uponSignal("ext").onInterface(intfName2).call([]() {
+   const char *intfName = "org.sdbuscpp.ext";
+   proxy->uponSignal("ext").onInterface(intfName).call([]() {
                                                           std::cerr << "Got Signal\n";
                                                           });
-   addProxy->finishRegistration();
+   proxy->finishRegistration();
 
-   std::cout << "Enterig into main loop\n";
+   std::cout << "Enterig into Event loop\n";
    connection->enterEventLoop();
 
    return 0;
