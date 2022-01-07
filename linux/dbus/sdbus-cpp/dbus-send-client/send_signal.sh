836c8e264494e30437ef0a284036457f91ad91b3
Amitesh Singh <singh.amitesh@gmail.com>
2022-01-07 12:58:23 +0530

    update the script

--- linux/dbus/sdbus-cpp/dbus-send-client/send_signal.sh
+++ linux/dbus/sdbus-cpp/dbus-send-client/send_signal.sh
@@ -1,2 +1,5 @@
-echo "dbus-send --system --type=signal / org.sdbuscpp.ext.ext"
+#both works
+#echo "dbus-send --system --type=signal / org.sdbuscpp.ext.ext"
+#dbus-send --system --type=signal / org.sdbuscpp.ext.ext
+echo "dbus-send --system --type=signal /org/sdbuscpp/ext org.sdbuscpp.ext.ext"
 dbus-send --system --type=signal /org/sdbuscpp/ext org.sdbuscpp.ext.ext
