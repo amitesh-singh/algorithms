2e3a7c83a617f0ee3fb8068cbb55d8abc2dc73e6
Amitesh Singh <singh.amitesh@gmail.com>
2022-01-07 18:29:07 +0530

    Add more signals

--- linux/dbus/sdbus-cpp/dbus-send-client/send_signal.sh
+++ linux/dbus/sdbus-cpp/dbus-send-client/send_signal.sh
@@ -1,3 +1,5 @@
-#both works
-echo "dbus-send --system --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext"
 dbus-send --system  --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext
+dbus-send --system  --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext2 string:"ext2"
+dbus-send --system  --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext3 string:"ext3" int32:5
+dbus-send --system  --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext4 string:"ext4" int32:5 array:int32:5,6,7
+dbus-send --system  --type=signal --dest=org.sdbuscpp.add /org/sdbuscpp/ext org.sdbuscpp.ext.ext5 string:"ext5" int32:5 array:byte:5,6,7
--- linux/dbus/sdbus-cpp/dbus-send-client/server.cpp
+++ linux/dbus/sdbus-cpp/dbus-send-client/server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <sdbus-c++/sdbus-c++.h>
 
 int main()
@@ -15,6 +16,25 @@ int main()
    const char *intfName = "org.sdbuscpp.ext";
    proxy->uponSignal("ext").onInterface(intfName).call([]() {
                                                           std::cerr << "Got Signal\n";
+                                                          });
+   proxy->uponSignal("ext2").onInterface(intfName).call([](const std::string &sig) {
+                                                          std::cerr << "Got Signal: " << sig << "\n";
+                                                          });
+   proxy->uponSignal("ext3").onInterface(intfName).call([](const std::string &sig, int x) {
+                                                          std::cerr << "Got Signal: " << sig << " x: " << x  << "\n";
+                                                          });
+   proxy->uponSignal("ext4").onInterface(intfName).call([](const std::string &sig, int x, std::vector<int> &v) {
+                                                          std::cerr << "Got Signal: " << sig << " x: " << x  << "\n";
+                                                          for (auto y: v)
+                                                            std::cout << y <<" ,";
+                                                            std::cout << '\n';
+                                                          });
+   proxy->uponSignal("ext5").onInterface(intfName).call([](const std::string &sig, int x, std::vector<uint8_t> &v) {
+                                                          std::cerr << "Got Signal: " << sig << " x: " << x  << "\n";
+                                                          std::cout << "array size: " << v.size() << "\n";
+                                                          for (uint8_t y: v)
+                                                            std::cout << int(y) <<" ,";
+                                                            std::cout << '\n';
                                                           });
    proxy->finishRegistration();
 
