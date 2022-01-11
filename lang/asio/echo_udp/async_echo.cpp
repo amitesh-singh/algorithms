0b7d29182017cfb1f3fb51b70255c083bef4a4f5
Amitesh Singh <singh.amitesh@gmail.com>
2022-01-11 10:04:09 +0530

    fix the async server

--- lang/asio/echo_udp/async_echo.cpp
+++ lang/asio/echo_udp/async_echo.cpp
@@ -1,30 +1,26 @@
 #include <asio.hpp>
 #include <iostream>
 
+std::uint16_t port = 15001;
+asio::io_context io;
+asio::ip::udp::endpoint receiver(asio::ip::udp::v4(), port);
+asio::ip::udp::socket socket_(io, receiver);
+char buf[1024];
+asio::ip::udp::endpoint sender;
+
+void callback(const std::error_code &, std::size_t bytes_transferred)
+{
+   std::cout << "message is recieved: message size is : " << bytes_transferred<< std::endl;
+   std::cout << sender << std::endl;;
+   socket_.send_to(asio::buffer(buf, bytes_transferred), sender);
+   socket_.async_receive_from(asio::buffer(buf, 1024), sender, callback);
+}
+
 int main()
 {
-   std::uint16_t port = 15001;
-   asio::io_context io;
-   asio::ip::udp::endpoint receiver(asio::ip::udp::v4(), port);
-   asio::ip::udp::socket socket(io, receiver);
+   socket_.async_receive_from(asio::buffer(buf, 1024), sender, callback);
 
-   for (;;)
-     {
-        char buf[1024];
-        asio::ip::udp::endpoint sender;
-
-        socket.async_receive_from(asio::buffer(buf, 1024), sender,
-                                  [&](const std::error_code &err, std::size_t bytes_transferred)
-                                  {
-                                  std::cout << "message is recieved: message size is : " << bytes_transferred<< std::endl;
-                                  socket.send_to(asio::buffer(buf, bytes_transferred), sender);
-                                  });
-
-        io.run();
-        io.reset();
-        std::cout << "polling\n";
-        //io.poll();
-     }
+   io.run();
 
    return 0;
 }
