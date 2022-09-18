9168dba5733c52d232f8caba11b3a334e91b1a71
Amitesh Singh <singh.amitesh@gmail.com>
2022-09-18 21:30:02 +0530

    Add more example

--- headfirstdp/signal-slot/main.cpp
+++ headfirstdp/signal-slot/main.cpp
@@ -10,8 +10,44 @@ class A
         }
 };
 
+class light
+{
+  public:
+      void on()
+        {
+           std::cout << "light is on\n";
+        }
+      void off()
+        {
+           std::cout << "light is off\n";
+        }
+};
+
+class btn
+{
+  public:
+      signal<> on;
+      signal<> off;
+      void upPress()
+        {
+           on.emit();
+        }
+      void downPress()
+        {
+           off.emit();
+        }
+};
+
 int main()
 {
+   btn b;
+   light l;
+   b.on.connect(std::bind(&light::on, std::ref(l)));
+   b.off.connect(std::bind(&light::off, std::ref(l)));
+
+   b.upPress();
+   b.downPress();
+
    signal<std::string, int> sig;
    sig.connect([](std::string arg1, int arg2) {
                std::cout << arg1 << ":" << arg2 << "\n";
@@ -25,5 +61,9 @@ int main()
                });
 
    sig.emit("the answer is", 42);
+
+   sig.connect(std::bind(&A::f, std::ref(aa), std::placeholders::_1, std::placeholders::_2));
+   sig.emit("meh", 20);
+
    return 0;
 }
