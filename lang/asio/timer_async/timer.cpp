b017d201983f07e0cd46dfc7cb0259ff1565b2fb
Amitesh Singh <singh.amitesh@gmail.com>
2022-09-25 11:59:40 +0530

    add recursive lambda timer code

--- lang/asio/timer_async/timer.cpp
+++ lang/asio/timer_async/timer.cpp
@@ -38,6 +38,23 @@ int main()
    //or 
    // t2.async_wait(std::bind(tick, asio_placeholders::error, std::ref(t2)));
 
+
+   //async timer implementation in a recursive lambda
+
+   asio::steady_timer t3(io);
+   
+   t3.expires_after(1s);
+   int n = 10;
+   auto recursive_lambda = [&t3, &n](auto &&recursive) {
+      if (n == 0) return;
+      std::cout << "n = " << n << '\n';
+      t3.expires_after(1s);
+      n--;
+      t3.async_wait(std::bind(recursive, recursive));
+   };
+
+   t3.async_wait(std::bind(recursive_lambda, recursive_lambda));
+
    io.run();
    return 0;
 }
