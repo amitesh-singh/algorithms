5e320b22e8460fca8ee4a69843fe023b0d9f88e4
Amitesh Singh <singh.amitesh@gmail.com>
2022-09-25 16:34:40 +0530

    Add more example

--- lang/cpp17/lambda/fold_expression.cpp
+++ lang/cpp17/lambda/fold_expression.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 
+//refer to https://www.fluentcpp.com/2021/03/12/cpp-fold-expressions/
 template<typename... T>
 auto do_stuff(T... t)
 {
@@ -14,10 +16,72 @@ auto do_stuff_lambda(T... t)
 {
     return ([t]() { return t + 3;}() + ...);
 }
+
+//handle no arguments
+template<typename... T>
+auto sum(T&&... args)
+{
+    return (0 + ... + args); //0 is the initial value.. so sum() would work
+}
+
+template<typename... T>
+auto sum2(T&&... args)
+{
+    return (args + ... + 0); // 0 is the initial value.. so sum2() would work
+}
+
+template<typename obj, typename... T>
+void callmemfunc(obj &o, T&&... args)
+{
+    ((o.*args)(), ...);
+}
+
+class A
+{
+    public:
+    void f1()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+    void f2()
+    {
+        std::cout << __PRETTY_FUNCTION__ << '\n';
+    }
+};
+
+auto add_all_strings_size(auto&& ... strings)
+{
+    std::cout << "size of the strings: " << sizeof...(strings) << std::endl;
+    return (0 + ... + strings.size());
+}
+
 int main()
 {
     std::cout << do_stuff(1, 2, 3, 4) << '\n';
     std::cout << do_stuff_lambda(1, 2, 3, 4, 5) << '\n';
+    std::cout << sum() << '\n';
+    std::cout << sum2() << '\n';
+
+
+    std::vector<int> v;
+    auto push_back = [](auto &&vector, auto&& ... args)
+    {
+        (vector.push_back(args), ...);
+    };
+
+    push_back(v, 1, 2, 3, 4);
+
+    for (const auto &n: v) {
+        std::cout << n << ',';
+    }
+    std::cout << '\n';
+
+    A aa;
+    callmemfunc(aa, &A::f1, &A::f2);
+    std::string s1 = "lol";
+    std::string s2 = "meh";
+
+    std::cout << add_all_strings_size(s1, s2) << '\n';
 
     return 0;
 }
