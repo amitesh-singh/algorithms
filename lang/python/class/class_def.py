b4ca76f545b0bad086d7400e2b895d2e8471eb37
Amitesh Singh <singh.amitesh@gmail.com>
2023-01-22 15:53:58 +0530

    python: make it compatible with py 3.X

--- lang/python/class/class_def.py
+++ lang/python/class/class_def.py
@@ -4,15 +4,20 @@ class Person(object):
     # by all instances of Person class.
     personCount = 0
     ## This is a constructor
-    def __init__(self):
-        print "Person() created."
+    def __init__(self, first, last):
+        print ("Person() created.")
         Person.personCount += 1
+        self.first = first
+        self.last = last
+
     ## This is a desctructor
     def __del__(self):
-        print "Person() is destroyed"
-        print self.__class__.__name__
+        print("Person() is destroyed")
+        print( self.__class__.__name__)
         Person.personCount -= 1
 
+    def getFullname(self):
+        return '{} {}'.format(self.first, self.last)
     def setAge(self, myage):
         self.age = myage
 
@@ -20,20 +25,20 @@ class Person(object):
         return self.age
 
 if __name__ == "__main__":
-    person = Person()
+    person = Person("aarav", "singh")
     person.setAge(10)
-    print "person age is:", person.getAge()
+    print ("person age is: {} ", person.getAge())
 
-    person2 = Person()
-    Person3 = Person()
+    person2 = Person("ami", "singh")
+    Person3 = Person("avi", "singh")
 
-    print "total persons are:", Person.personCount
+    print( "total persons are: {} ", Person.personCount)
     # prints the class doc
-    print Person.__doc__
-    print Person.__name__
-    print Person.__module__
-    print Person.__bases__
-    print Person.__dict__
+    print (Person.__doc__)
+    print (Person.__name__)
+    print (Person.__module__)
+    print (Person.__bases__)
+    print (Person.__dict__)
 
     del person
     del person2
