9f6e4abb38f265e76de4dd0abf5f62b50fecf273
Amitesh Singh <singh.amitesh@gmail.com>
2023-04-08 06:23:07 +0530

    add more example

--- linux/dbus/sdbus-cpp/dbus_query/main.cpp
+++ linux/dbus/sdbus-cpp/dbus_query/main.cpp
@@ -15,15 +15,15 @@ int main()
    const char *objPath = "/org/freedesktop/DBus";
 
    auto obj = sdbus::createProxy("org.freedesktop.DBus", objPath);
-   obj->uponSignal("NameAcquired").onInterface(interface).call([](std::string  str) {
-                                                               std::cout << "added " << str << std::endl;
+   obj->uponSignal("NameAcquired").onInterface(interface).call([](const std::string &str) {
+                                                               std::cout << "NameAcquired " << std::endl;
                                                                });
 
-   obj->uponSignal("NameLost").onInterface(interface).call([](std::string  str) {
-                                                               std::cout << "added " << str << std::endl;
+   obj->uponSignal("NameLost").onInterface(interface).call([](const std::string  &str) {
+                                                               std::cout << "NameLost " << str << std::endl;
                                                                });
-   obj->uponSignal("NameOwnerChanged").onInterface(interface).call([](std::string  str) {
-                                                               std::cout << "added " << str << std::endl;
+   obj->uponSignal("NameOwnerChanged").onInterface(interface).call([](const std::string  &name, const std::string &from, const std::string &to) {
+                                                               std::cout << "NameOwnerChanged: name: " << name << " from:" << from << " to:" << to << std::endl;
                                                                });
    obj->finishRegistration();
 
