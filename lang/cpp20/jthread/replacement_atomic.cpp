4f5244ad845ef52fab5aacf7f973730880731c10
Amitesh Singh <singh.amitesh@gmail.com>
2023-04-19 22:28:03 +0530

    add jthread example

---
+++ lang/cpp20/jthread/replacement_atomic.cpp
@@ -0,0 +1,37 @@
+#include <thread>
+#include <iostream>
+#include <chrono>
+#include <atomic>
+
+using namespace std::string_literals;
+class A
+{
+    std::jthread t;
+    public:
+    A()
+    {
+        t = std::jthread([this](std::stop_token st) {
+            while (!st.stop_requested())
+            {
+                std::cout << "..\n";
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            }
+        });
+    }
+    ~A()
+    {
+       //no need to call
+       //t.request_stop();
+       // or join the thread
+       // if (t.joinable()) {
+       //   t.join();
+       // }
+    }
+};
+
+int main()
+{
+    A aa;
+    std::this_thread::sleep_for(std::chrono::seconds(4));
+    return 0;
+}
