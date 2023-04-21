b4f99bd5a4f80bdfed4af8bcbdda70df6f8404b4
Amitesh Singh <singh.amitesh@gmail.com>
2023-04-21 19:48:18 +0530

    Add better example of template lambda

--- lang/cpp20/lambda/template_lambda.cpp
+++ lang/cpp20/lambda/template_lambda.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
-int main()
+auto main()->int
 {
-    auto sum = []<typename T, typename U>(const T& a, co)
+    auto sum = []<typename T, typename U>(const T& a, const U& b) {
+         return a + b;
+    };
+
+    std::cout << sum(1, 1) << "," << sum(0.1, 1) << sum(2, 3) << std::endl;
+
+    auto sum2 = [](auto&&... args) {
+         return (args + ...);
+    };
+
+    std::cout << sum2(1, 1, 2.2) << "," << sum2(2, 4, 5.5, 5.5) << std::endl;
+    return 0;
 }
