4d82b59b91c25c2bb31fa8e3d53dcc37a5c8d073
Amitesh Singh <singh.amitesh@gmail.com>
2023-09-20 09:35:33 +0530

    make it better

--- lang/cpp20/concepts/check_memberfunc.cpp
+++ lang/cpp20/concepts/check_memberfunc.cpp
@@ -35,12 +35,33 @@ class A
     }
 };
 
+
+template<typename LanguagePolicy>
+class B
+{
+    public:
+    static_assert(std::is_same_v<decltype(LanguagePolicy().message()), std::string>,
+                  "LanguagePolicy must provide a Message() function that returns a std::string.");
+    //below will work without the need of default constructor
+    // e.g. class LanguagePolicy
+    //      {
+    //          public: 
+    //           LanguagePolicy() = delete;
+    //      };
+    //
+    static_assert(std::is_same_v<decltype(std::declval<LanguagePolicy>().message()), std::string>,
+                  "LanguagePolicy must provide a Message() function that returns a std::string.");
+};
+
 int main()
 {
     A<LanguagePolicyEnglish> a;
 
     a.print();
 
-    ///A<LanguagePolicyNothing> b; //compilation error
+    ///A<LanguagePolicyNothing> b; //compilation errors
+    B<LanguagePolicyEnglish> bb;
+    //B<LanguagePolicyNothing> bb2; //compilation errors
+
     return 0;
 }
