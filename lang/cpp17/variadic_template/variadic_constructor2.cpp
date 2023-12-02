31a7139deb520f7749fccc0354c44fdcb563e705
Amitesh Singh <singh.amitesh@gmail.com>
2023-12-02 19:38:34 +0530

    add generic auto

--- lang/cpp17/variadic_template/variadic_constructor2.cpp
+++ lang/cpp17/variadic_template/variadic_constructor2.cpp
@@ -21,7 +21,30 @@ public:
     }
 };
 
-int main() {
+//using auto which is like replacement of templates
+struct var2
+{
+    var2(auto&&... args)
+    {
+        parse_args(std::forward<decltype(args)>(args)...);
+    }
+
+    private:
+        void parse(auto&& arg)
+        {
+            std::cout << arg << std::endl;
+        }
+        void parse_args(auto&&... args)
+        {
+            (parse(args), ...);
+        }
+};
+
+int main()
+{
     var v(10, 20, "hello");
+    
+    var2 v2(10, 20, "hey");
+    
     return 0;
 }
