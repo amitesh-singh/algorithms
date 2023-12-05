88627e49ca4bd1f0c07052f5ca52a97d6f487612
Amitesh Singh <singh.amitesh@gmail.com>
2023-12-05 09:18:23 +0530

    add visitor pattern using std::visit and std::varient

---
+++ lang/cpp17/variant/visitor_pattern.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <variant>
+#include <vector>
+
+/*
+Good thing is that in traditional visitor pattern, derived classes (circle, rectangle)
+has to be derived from an interface (e.g. shape) but with std::visit and std::variant,
+we don't need to.
+
+*/
+class circle
+{
+public:
+    int r;
+    void rotate(int angle)
+    {
+        std::cout << "rotate circle\n";
+    }
+};
+
+class rectangle
+{
+public:
+    int x, y, w, h;
+    void rotate(int angle)
+    {
+        std::cout << "rotate rectangle\n";
+    }
+};
+
+using shape = std::variant<circle, rectangle>;
+
+struct draw
+{
+    void operator() (circle &c)
+    {
+        std::cout << "drawing circle: " << c.r << std::endl;
+    }
+    void operator() (rectangle &r)
+    {
+        std::cout << "drawing rectangle: at (" << r.x << ", " << r.y << ")" << std::endl;
+    }
+};
+
+struct rotate
+{
+    int angle;
+    void operator() (circle &c)
+    {
+        c.rotate(angle);
+    }
+    void operator() (rectangle &r)
+    {
+        r.rotate(angle);
+    }
+};
+
+int main()
+{
+    std::vector<shape> shapes = {
+        circle {2},
+        rectangle{0, 0, 1, 1},
+        circle{3}
+    };
+
+    for (auto &s: shapes) {
+        std::visit(draw{}, s);
+        std::visit(rotate{20}, s);
+    }
+
+    return 0;
+}
